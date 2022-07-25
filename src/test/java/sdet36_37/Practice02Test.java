c00835157c785ce80bd6fe4428777c927d73cf2d
snanilsn94@gmail.com <snanilsn94@gmail.com>
2022-07-25 11:51:15 +0530

    my first commit

--- src/test/java/sdet36_37/Practice02Test.java
+++ src/test/java/sdet36_37/Practice02Test.java
@@ -20,6 +20,7 @@ public class Practice02Test {
 	@Test(enabled=false)
 	public void test2() {
 		System.out.println("From Tyss002");
+		System.out.println("welcome anil");
 	}
 	
 	@Test(priority=1)
